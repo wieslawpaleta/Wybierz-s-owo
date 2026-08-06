0f9d1a5b33e5d2ed4159836a6823826596fcb59c
wieslawpaleta <tankluk@gmail.com>
2026-08-06 20:17:04 +0200

    Poprawiłem nieco menu główne

--- Program/Główne_okno.py
+++ Program/Główne_okno.py
@@ -4,13 +4,26 @@
 
 print("Cześć! wybierz tryb, aby przejść dalej do programu.")
 tryb = input("Wybierz tryb \"Losowanie\" albo \"Wybierz słowo\" albo \"Wyjście\": ")
+while True:
+    try:
+        if tryb.lower() == "losowanie":
+            import Losowanie_słów
+            break
+        elif tryb.lower() == "wybierz słowo":
+            import Wybierz_słowo2
+            break
+        elif tryb.lower() == "wyjście":
+            print("Do zobaczenia!")
+            exit()
+            break
+        else:
+            print("Masz ostatnie ostrzeżenie w tej chwili, \nmasz ostatnie ostrzeżenie!")
+            tryb = input("Wybierz tryb \"Losowanie\" albo \"Wybierz słowo\" albo \"Wyjście\": ")
+    except ValueError:
+        print("Posłuchaj dzieciaku!")
 
-if tryb.lower() == "losowanie":
-    import Losowanie_słów
-elif tryb.lower() == "wybierz słowo":
-    import Wybierz_słowo2
-elif tryb.lower() == "wyjście":
-    print("Do zobaczenia!")
-    exit()
-else:
-    print("Niepoprawny tryb. Wybierz \"Losowanie\" albo \"Wybierz słowo\".")
+
+
+     
+
+
