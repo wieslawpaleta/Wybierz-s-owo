d6795a79791e9773d1cd38553c45d377c2674507
wieslawpaleta <tankluk@gmail.com>
2026-08-11 20:44:08 +0200

    Utworzyłem prawidłową pętlę

--- Program/Główne_okno.py
+++ Program/Główne_okno.py
@@ -1,29 +1,29 @@
 
+import Losowanie_słów
+import Wybierz_słowo3
 
+def Główne_okno():
+    print("Cześć! wybierz tryb, aby przejść dalej do programu.")
+    tryb = input("Wybierz tryb \"Losowanie\" albo \"Wybierz słowo\" albo \"Wyjście\": ")
+    while True:
+        try:
+            if tryb.lower() == "losowanie":
+                return Losowanie_słów.losuj_slowo()
+                
+            elif tryb.lower() == "wybierz słowo":
+                return Wybierz_słowo3.wybierz_slowo()
+            
+            elif tryb.lower() == "wyjście":
+                print("Do zobaczenia!")
+                exit()
+            else:
+                print("Masz ostatnie ostrzeżenie w tej chwili, \nmasz ostatnie ostrzeżenie!")
+                tryb = input("Wybierz tryb \"Losowanie\" albo \"Wybierz słowo\" albo \"Wyjście\": ")
+        except ValueError:
+            print("Posłuchaj dzieciaku!")
 
-
-print("Cześć! wybierz tryb, aby przejść dalej do programu.")
-tryb = input("Wybierz tryb \"Losowanie\" albo \"Wybierz słowo\" albo \"Wyjście\": ")
-while True:
-    try:
-        if tryb.lower() == "losowanie":
-            import Losowanie_słów
-            break
-        elif tryb.lower() == "wybierz słowo":
-            import Wybierz_słowo2
-            break
-        elif tryb.lower() == "wyjście":
-            print("Do zobaczenia!")
-            exit()
-            break
-        else:
-            print("Masz ostatnie ostrzeżenie w tej chwili, \nmasz ostatnie ostrzeżenie!")
-            tryb = input("Wybierz tryb \"Losowanie\" albo \"Wybierz słowo\" albo \"Wyjście\": ")
-    except ValueError:
-        print("Posłuchaj dzieciaku!")
-
-
-
+if __name__ == "__main__":
+    Główne_okno() 
      
 
 
