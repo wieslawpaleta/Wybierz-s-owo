d4d3d7d1519713f8ee63074cd4c22fe23b753235
wieslawpaleta <tankluk@gmail.com>
2026-09-03 20:54:54 +0200

    Add files via upload

--- edytor_słowników.py
+++ edytor_słowników.py
@@ -10,15 +10,15 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SCIEZKA_JSON = os.path.join(BASE_DIR, "lista_palabras.json")
 
 
-#Tutaj jest zapisany kod, który dodaje słowo do zbioru słów.
+#Tutaj jest zapisany kod, który dodaje, usuwa słowo, wyświetla zbiór słów, wraca do głównego okna oraz kończy działanie programu.
 def edytor_zbioru():
 
 
     while True:
 
-        cochceszzrobić = input("\nPokaż listę słów albo dodaj lub usuń słowo ze zbioru słów. Wpisz \"Pokaż\" albo \"Dodaj\" albo \"Usuń\" albo \"Powrót\":")
+        cochceszzrobić = input("\nWpisz:\n1) \"Wyświetl\",aby wyświetlić zbiór,\n2) \"Dodaj\",aby dodać słowo,\n3) \"Usuń\",aby usunąć słowo,\n4) \"Powrót\",aby wrócić do głównego menu:")
 
-        if cochceszzrobić.lower() == "pokaż":
+        if cochceszzrobić.lower() == "wyświetl":
             print("\nLista słów:")
 
             słowa_lista = list(zbiór_słów)
@@ -31,34 +31,22 @@ def edytor_zbioru():
 
         elif cochceszzrobić.lower() == "dodaj":
 
-            początek = input("\nCzy chcesz dodać nowe słowo do zgadywanki? (tak/nie): ")
+
+            dodatek = input("\nWprowadź słowe, które chcesz dodać: ")
+            zbiór_słów.append(dodatek)
+            print("\nSłowo zostało dodane")
 
 
-            if początek.lower() == "tak":
-                dodatek = input("\nWprowadź słowe, które chcesz dodać: ")
-                zbiór_słów.append(dodatek)
-                print("\nSłowo zostało dodane")
+            with open(SCIEZKA_JSON, "w", encoding="utf-8") as archivo:
+                json.dump(zbiór_słów, archivo, ensure_ascii=False, indent=4)
 
 
-                with open(SCIEZKA_JSON, "w", encoding="utf-8") as archivo:
-                    json.dump(zbiór_słów, archivo, ensure_ascii=False, indent=4)
-
-
-                print("\nLista actualizada y guardada en lista_palabras.json")
-
-
-
-            elif początek.lower() == "nie":
-                print("\nNo to wracamy do głównego menu!\n")
-                return główne_okno.główne_okno()
-                
-
-
-            else:
-                print("Niepoprawna odpowiedź. Wpisz 'tak' lub 'nie'.\n")
+            print("\nLista actualizada y guardada en lista_palabras.json")
 
 
         elif cochceszzrobić.lower() == "usuń":
+
+
             usuwanie = input("\nWprowadź słowo, które chcesz usunąć: ")
             if usuwanie in zbiór_słów:
                 zbiór_słów.remove(usuwanie)
@@ -72,12 +60,18 @@ def edytor_zbioru():
                 print("\nLista actualizada y guardada en lista_palabras.json")
 
 
+            elif usuwanie not in zbiór_słów:
+                print("\nNie znaleziono słowa w zbiorze.")
+                
 
-            else:
-                print("\nNie znaleziono słowa w zbiorze.\n")
+
+        elif cochceszzrobić.lower() == "powrót":
+            print("\nNo to wracamy!\n")
+            return główne_okno.główne_okno()
+        
 
         else:  
-            return główne_okno.główne_okno()
+            print("\nNiepoprawna opcja. Spróbuj ponownie.")
 
 
 
