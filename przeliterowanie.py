d1103ea99e07e698bd72a03c6da1eb243693c762
wieslawpaleta <tankluk@gmail.com>
2026-09-10 21:40:18 +0200

    Add files via upload

--- przeliterowanie.py
+++ przeliterowanie.py
@@ -1,15 +1,17 @@
+#W tym miejscu można przeliterować wybrane słowo.
 import główne_okno
 
 
 def przeliteruj():
-    print("PRZELITERUJ SŁOWO!\n")
+    print("\nPRZELITERUJ SŁOWO!\n")
     while True:
         print("-" * 10)
-        print("Dostępne komendy:")
-        print("1) \"Pwrt\", żeby wrócić do głównego okna.")
-        print("2) \"Wsc\", żeby zakończyć działanie programu.")
+        print("Dostępne opcje:")
+        print("1) Wpisz słowo, które chcesz przeliterować,")
+        print("2) Wpisz \"Pwrt\", żeby wrócić do głównego okna,")
+        print("3) Wpisz \"Wsc\", żeby zakończyć działanie programu.")
         print("-" * 10)
-        rozkład = input("Podaj słowo, które chcesz przeliterować: ")
+        rozkład = input("Co zamierzasz? ")
 
 
         if rozkład.lower() == "pwrt":
@@ -23,13 +25,11 @@ def przeliteruj():
 
 
         elif rozkład.isalpha():
+            print("")    
             for x in rozkład:
                 print(x)
-
-
-        
-
-        
+            print("")
+ 
         else:
             print("Spróbuj jeszcze raz.")
         
